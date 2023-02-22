926670b2605d7e86c15acd45a1007c37059bb338
Vivy <Tomitaviorel@yahoo.com>
2023-02-22 13:32:33 +0200

    feat:overflow hidden

--- src/component/articlelist/articlecontent.jsx
+++ src/component/articlelist/articlecontent.jsx
@@ -5,27 +5,29 @@ const ArticleContent = ({ art }) => {
   return (
     <div>
       <div className='art__content'>
-        <h1>{art.title}</h1>
-        <h4>{art.subTitle}</h4>
-        <img src={art.img[0]} alt='picture' />
-        <div className='art__user'>
-          <img src={art.user.profilePicture} alt='userpic' />
-          <p>{art.user.name}</p>
-          <p>{art.user.id}</p>
-          <p>{art.creationDate}</p>
-        </div>
-        <div className='art__text'>
-          {art.info.map((text) => (
-            <p>{text}</p>
-          ))}
-        </div>
-        <button className='art__btn'> View All Comments</button>
-        <div className='art__tags'>
-          {art.tag.map((tags) => (
-            <a className='art__anchor' href=''>
-              {tags}
-            </a>
-          ))}
+        <div className='art__wrapper'>
+          <h1>{art.title}</h1>
+          <h4>{art.subTitle}</h4>
+          <img src={art.img[0]} alt='picture' />
+          <div className='art__user'>
+            <img src={art.user.profilePicture} alt='userpic' />
+            <p>{art.user.name}</p>
+            <p>{art.user.id}</p>
+            <p>{art.creationDate}</p>
+          </div>
+          <div className='art__text'>
+            {art.info.map((text) => (
+              <p>{text}</p>
+            ))}
+          </div>
+          <button className='art__btn'> View All Comments</button>
+          <div className='art__tags'>
+            {art.tag.map((tags) => (
+              <a className='art__anchor' href=''>
+                {tags}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </div>
